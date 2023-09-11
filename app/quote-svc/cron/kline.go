b6f70bbf5b5ccd3a40abf9ea94445d38eb7ab96f
yuanshi007 <103150111@qq.com>
2023-09-12 02:05:54 +0800

    修复Usercenter服务发现 新增容器MODE环境变量

---
+++ app/quote-svc/cron/kline.go
@@ -0,0 +1,43 @@
+package cron
+
+import "encoding/json"
+
+var RateKey = "rate:usd-rmb"
+
+type Ticker struct {
+	Symbol   string  `json:"symbol"`
+	Last     float64 `json:"last"`
+	Buy      float64 `json:"buy"`
+	Open     float64 `json:"open"`
+	Sell     float64 `json:"sell"`
+	High     float64 `json:"high"`
+	Low      float64 `json:"low"`
+	Vol      float64 `json:"vol"`
+	QuoteVol float64 `json:"quoteVol"`
+	Change   string  `json:"change"`
+	Date     uint64  `json:"date"` // 单位:ms
+}
+type Kline struct {
+	Symbol    string  `json:"symbol"`
+	Open      float64 `json:"open"`
+	Sell      float64 `json:"sell"`
+	High      float64 `json:"high"`
+	Low       float64 `json:"low"`
+	Vol       float64 `json:"vol"`
+	CloseTime uint64  `json:"time"`
+	QuoteVol  float64 `json:"amount"`
+}
+
+func (tk Ticker) MarshalBinary() ([]byte, error) {
+	return json.Marshal(tk)
+}
+
+type QuoteRate struct {
+	InstrumentID string `json:"instrument_id"`
+	Rate         string `json:"rate"`
+	Timestamp    string `json:"timestamp"`
+}
+
+func (rate QuoteRate) MarshalBinary() ([]byte, error) {
+	return json.Marshal(rate)
+}
